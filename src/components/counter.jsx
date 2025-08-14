10dd563e28d44ab833c520fd54632b6b03b96edb
meron847 <belachewmeron15@gmail.com>
2025-08-14 10:50:39 +0100

    Rendering classes Dunamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
    state = {
-    count:0,
-    imageUrl:"https://picsum.photos/200"
+    count:1
+   
    };
     render() { 
+      
         return (
-        <div>
-            <img src={this.state.imageUrl} alt=""/>
-            <span className='badge badge-primary m-2'>{this.formatCount()}</span>
-            <botton className="btn btn-secondary btn-sm">Increment</botton>
+        <div> 
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
             );
     }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
     formatCount() {
         const { count } = this.state;
         return count === 0 ? "Zero" : count;
